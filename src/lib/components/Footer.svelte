56b7c847469d8fa30df57f6e2bef42f2c62f6b0d
Viggi <viggibot@gmail.com>
2026-08-05 13:22:33 +0530

    refund added in footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -38,6 +38,7 @@
 					<h4>Legal</h4>
 					<a href="/terms">Terms of Service</a>
 					<a href="/privacy">Privacy Policy</a>
+					<a href="/refunds">Refund Policy</a>
 				</div>
 			</nav>
 		</div>
